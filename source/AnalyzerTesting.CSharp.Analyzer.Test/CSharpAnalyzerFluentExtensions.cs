dde77b39b55baf9e8b5e7c67a6db357d882ec0d8
tom-englert <mail@tom-englert.de>
2023-04-19 12:54:51 +0200

    Add documentation for the methods

--- source/AnalyzerTesting.CSharp.Analyzer.Test/CSharpAnalyzerFluentExtensions.cs
+++ source/AnalyzerTesting.CSharp.Analyzer.Test/CSharpAnalyzerFluentExtensions.cs
@@ -10,8 +10,15 @@ namespace AnalyzerTesting.CSharp.Analyzer.Test;
 
 using Verifier = global::Microsoft.CodeAnalysis.Testing.Verifiers.MSTestVerifier;
 
-public static class CSharpAnalyzerFluentExtensionMethods
+internal static class CSharpAnalyzerFluentExtensions
 {
+    /// <summary>
+    /// Adds nuget package references to the test compilation
+    /// </summary>
+    /// <typeparam name="TTest">The test type</typeparam>
+    /// <param name="test">The test</param>
+    /// <param name="packages">The packages to reference</param>
+    /// <returns>The test</returns>
     public static TTest AddPackages<TTest>(this TTest test, params PackageIdentity[] packages)
         where TTest : AnalyzerTest<Verifier>
     {
@@ -19,13 +26,27 @@ public static class CSharpAnalyzerFluentExtensionMethods
         return test;
     }
 
-    public static TTest AddDiagnostics<TTest>(this TTest test, params DiagnosticResult[] expected)
+    /// <summary>
+    /// Adds the diagnostics to the list of expected diagnostics
+    /// </summary>
+    /// <typeparam name="TTest">The test type</typeparam>
+    /// <param name="test">The test</param>
+    /// <param name="expected">The diagnostics</param>
+    /// <returns>The test</returns>
+    public static TTest AddExpectedDiagnostics<TTest>(this TTest test, params DiagnosticResult[] expected)
         where TTest : AnalyzerTest<Verifier>
     {
         test.ExpectedDiagnostics.AddRange(expected);
         return test;
     }
 
+    /// <summary>
+    /// Adds a solution transform to the test compilation
+    /// </summary>
+    /// <typeparam name="TTest">The test type</typeparam>
+    /// <param name="test">The test</param>
+    /// <param name="transform">The transforms to add</param>
+    /// <returns>The test</returns>
     public static TTest AddSolutionTransform<TTest>(this TTest test, Func<Solution, Project, Solution> transform)
         where TTest : AnalyzerTest<Verifier>
     {
@@ -38,6 +59,13 @@ public static class CSharpAnalyzerFluentExtensionMethods
         return test;
     }
 
+    /// <summary>
+    /// Adds source files to the test compilation
+    /// </summary>
+    /// <typeparam name="TTest">The test type</typeparam>
+    /// <param name="test">The test</param>
+    /// <param name="sources">The sources to add</param>
+    /// <returns>The test</returns>
     public static TTest AddSources<TTest>(this TTest test, params string[] sources)
         where TTest : AnalyzerTest<Verifier>
     {
@@ -49,6 +77,13 @@ public static class CSharpAnalyzerFluentExtensionMethods
         return test;
     }
 
+    /// <summary>
+    /// Adds assembly references to the test compilation
+    /// </summary>
+    /// <typeparam name="TTest">The test type</typeparam>
+    /// <param name="test">The test</param>
+    /// <param name="localReferences">The assemblies to reference</param>
+    /// <returns>The test</returns>
     public static TTest AddReferences<TTest>(this TTest test, params Assembly[] localReferences)
         where TTest : AnalyzerTest<Verifier>
     {
@@ -66,6 +101,13 @@ public static class CSharpAnalyzerFluentExtensionMethods
         return test;
     }
 
+    /// <summary>
+    /// Sets the reference assemblies for the test compilation.
+    /// </summary>
+    /// <typeparam name="TTest">The test type</typeparam>
+    /// <param name="test">The test</param>
+    /// <param name="referenceAssemblies">The reference assemblies to use</param>
+    /// <returns>The test</returns>
     public static TTest WithReferenceAssemblies<TTest>(this TTest test, ReferenceAssemblies referenceAssemblies)
         where TTest : AnalyzerTest<Verifier>
     {
@@ -73,13 +115,27 @@ public static class CSharpAnalyzerFluentExtensionMethods
         return test;
     }
 
-    public static TTest WithLangVersion<TTest>(this TTest test, LanguageVersion languageVersion)
+    /// <summary>
+    /// Sets the language version used by the test compilation.
+    /// </summary>
+    /// <typeparam name="TTest">The test type</typeparam>
+    /// <param name="test">The test</param>
+    /// <param name="languageVersion">The language version to use</param>
+    /// <returns>The test</returns>
+    public static TTest WithLanguageVersion<TTest>(this TTest test, LanguageVersion languageVersion)
         where TTest : AnalyzerTest<Verifier>
     {
         test.SolutionTransforms.Add((solution, projectId) => solution.WithProjectParseOptions(projectId, new CSharpParseOptions(languageVersion, DocumentationMode.Diagnose)));
         return test;
     }
 
+    /// <summary>
+    /// Updates the compilation options of the test compilation.
+    /// </summary>
+    /// <typeparam name="TTest">The test type</typeparam>
+    /// <param name="test">The test</param>
+    /// <param name="callback">The callback to update the compilation options</param>
+    /// <returns>The test</returns>
     public static TTest WithProjectCompilationOptions<TTest>(this TTest test, Func<CSharpCompilationOptions, CSharpCompilationOptions> callback)
         where TTest : AnalyzerTest<Verifier>
     {
--- source/AnalyzerTesting.CSharp.Extensions/ExtensionMethods.cs
+++ source/AnalyzerTesting.CSharp.Extensions/ExtensionMethods.cs
@@ -7,20 +7,46 @@ namespace AnalyzerTesting.CSharp.Extensions;
 
 public static class ExtensionMethods
 {
+    /// <summary>
+    /// Converts a <see cref="DiagnosticDescriptor"/> into a <see cref="DiagnosticResult"/>
+    /// </summary>
+    /// <param name="descriptor">The <see cref="DiagnosticDescriptor"/></param>
+    /// <returns>The new <see cref="DiagnosticResult"/></returns>
     public static DiagnosticResult AsResult(this DiagnosticDescriptor descriptor) => new(descriptor);
 
+    /// <summary>
+    /// Up-casts <see cref="CompilationOptions"/> to <see cref="CSharpCompilationOptions"/>
+    /// </summary>
+    /// <param name="options">The <see cref="CompilationOptions"/></param>
+    /// <returns>The <see cref="CSharpCompilationOptions"/></returns>
     public static CSharpCompilationOptions AsCSharpCompilationOptions(this CompilationOptions options) => (CSharpCompilationOptions)options;
 
+    /// <summary>
+    /// Adds C# defaults to the compilation options: Enable NRTs and set NullableWarningsAsErrors.
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
     public static CSharpCompilationOptions WithCSharpDefaults(this CompilationOptions options) => options
         .AsCSharpCompilationOptions()
         .WithNullableWarningsAsErrors()
         .WithNullableContextOptions(NullableContextOptions.Enable);
 
+    /// <summary>
+    /// Adds nuget packages to the <see cref="ReferenceAssemblies"/>
+    /// </summary>
+    /// <param name="referenceAssemblies">The <see cref="ReferenceAssemblies"/></param>
+    /// <param name="packages">The packages to add</param>
+    /// <returns>The updated <see cref="ReferenceAssemblies"/></returns>
     public static ReferenceAssemblies AddPackages(this ReferenceAssemblies referenceAssemblies, params PackageIdentity[] packages)
     {
         return referenceAssemblies.AddPackages(packages.ToImmutableArray());
     }
 
+    /// <summary>
+    /// Sets the severity of NRT warnings to error.
+    /// </summary>
+    /// <param name="options">The <see cref="CSharpCompilationOptions"/></param>
+    /// <returns>The updated <see cref="CSharpCompilationOptions"/></returns>
     public static CSharpCompilationOptions WithNullableWarningsAsErrors(this CSharpCompilationOptions options)
     {
         return options.WithSpecificDiagnosticOptions(options.SpecificDiagnosticOptions.SetItems(NullableWarningsAsErrors));
